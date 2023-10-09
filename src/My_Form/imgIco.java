efcaa506332294dff8e881d7e7c6ad030618e3a4
berliani risqi <berlianirisqi1712@gmail.com>
2023-10-09 12:54:27 +0700

    Update 1

---
+++ src/My_Form/imgIco.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package My_Form;
+
+/**
+ *
+ * @author linad
+ */
+class imgIco {
+    
+    public static void main(String[] args){
+        LoginForm LoginFormFrame = new LoginForm();
+        LoginFormFrame.setVisible(true);
+        LoginFormFrame.setLocationRelativeTo(null);
+         
+    
+}
+}
